f68338ea005d6cfde5b6528c140960bdaf09a74d
LolloMira <lorenzo23082000@gmail.com>
2022-02-24 12:10:52 +0100

    ...

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 
+// setTimeout(() => {
+//    console.log("Pippo"); 
+// }, 10000);
+
+// let intervalCounter = 0;
+//  const interval = setInterval(() => {
+//     console.log(intervalCounter);
+//     if (intervalCounter === 10) {
+//         clearInterval(interval);
+//     }
+//     intervalCounter++
+// }, 1000);
+
+
 const button3 = document.getElementById('button3')
 
 button3.onclick = () => buttonClick('button 3');
@@ -22,6 +36,15 @@ document.addEventListener('keydown', changeBackground);
 const link = document.getElementById('link');
 
 link.addEventListener('click', displayPar)
+
+
+const div = document.getElementById('mouse-div-2');
+
+// div.addEventListener('mouseenter', onMouseEnter);
+
+// div.addEventListener('mouseleave', onMouseLeave);
+
+div.addEventListener('mousemove', onMouseOver);
 
 function buttonClick(string) {
     console.log(string);
@@ -54,4 +77,40 @@ function changeBackground(event){
         event.preventDefault(); 
         document.body.style.backgroundColor = '#'+number+number+number+number+number+number
     }
+}
+
+
+function onMouseEnter() {
+    console.log("Sono entrato");
+}
+
+
+function onMouseLeave() {
+    console.log("Sono uscito");
+}
+
+
+function onMouseOver() {
+    console.log("Sono sopra");
+}
+
+
+
+let selectedKey = ""
+function onKeyDown(event) {
+    if (event.key !== selectedKey) {
+        console.log("Tasto premuto" + event.key);
+        selectedKey = event.key;
+    }
+    
+}
+
+let searchTimeout;
+function realTimeSearch(event) {
+    if (searchTimeout) {
+        clearTimeout(searchTimeout);
+    }
+    const searchTimeout = setTimeout(() => {
+        console.log("Sto cercando");
+    }, 1000);
 }
